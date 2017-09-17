10d68a7612035101ac39f9379bab091fc5291cbd
zhaohejing <454258314@qq.com>
2017-09-17 12:28:13 +0800

    完善

--- src/YT.Web/Assets/src/components/SidebarItem.vue
+++ src/YT.Web/Assets/src/components/SidebarItem.vue
@@ -3,19 +3,18 @@
     <nav class="sidebar-nav">
       <ul class="nav">
         <template v-for="item in routes">
-          <router-link :key="item.name" tag="li" class="nav-item nav-dropdown" v-if="item.children&&item.children.length>0" :to="item.url" disabled>
-
+          <router-link :key="item.name" tag="li" class="nav-item nav-dropdown" v-if="item.children&&item.children.length>0"
+             to="" disabled>
             <div class="nav-link nav-dropdown-toggle" @click="handleClick">
               <Icon :type="item.icon" />{{ item.displayName}} </div>
-
             <ul class="nav-dropdown-items">
-
               <li class="nav-item" :key="child.name" v-for="child in item.children" @click="addActive">
                 <router-link :to="child.url" class="nav-link">
                   <Icon :type="child.icon" />{{ child.displayName}} </router-link>
               </li>
             </ul>
           </router-link>
+          
           <li class="nav-item" :key="item.name" v-if="!item.children||item.children.length<=0">
             <router-link :to="item.url" class="nav-link" exact>
               <Icon :type="item.icon" />{{ item.displayName}} </router-link>
